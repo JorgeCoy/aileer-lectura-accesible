e1a5a7510b8d41b790c94e47a84b0b0325de4496
Jorge Coy <jorgecoy@gmail.com>
2026-08-31 21:05:21 -0500

    refactor(cleanup): remove unused dead views and theme contexts

--- src/views/AdultView.jsx
+++
@@ -1,9 +0,0 @@
-// src/views/AdultView.jsx
-import React from "react";
-import GenericReadingView from "./GenericReadingView";
-
-const AdultView = ({ moduleContext }) => {
-  return <GenericReadingView modeId="adult" moduleContext={moduleContext} />;
-};
-
-export default AdultView;
